bf2163c82acef6dd1ec968bdc343bb40df507883
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-04-29 15:25:21 +0100

    Tech stack area styling and icons

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,5 +1,5 @@
 import { AiFillHtml5, AiFillGithub } from "react-icons/ai";
-import { DiCss3, DiGit } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
 import {
   SiJavascript,
   SiBootstrap,
@@ -12,33 +12,38 @@ import {
   SiUnity,
   SiGit,
   SiGatsby,
+  SiMaterialUi,
+  SiPostgresql,
 } from "react-icons/si";
 import { FaReact, FaAws } from "react-icons/fa";
 function TechStack({ colour }) {
   return (
-    <summary>
+    <summary className='tech-summary' >
       <h2>Tech Stack</h2>
       <p>I have experience with the following technologies:</p>
-      <AiFillHtml5 />
-      <DiCss3 />
-      <SiJavascript />
-      <FaReact />
-      <SiRedux />
-      <SiBootstrap />
-      <FaAws />
-      <SiGit />
-      <AiFillGithub />
-      <SiFirebase />
-      <SiNetlify />
-
-      <p>
-        I am Currently learning the following:
+      <div className="tech-icons">
+        <AiFillHtml5 />
+        <DiCss3 />
+        <SiJavascript />
+        <FaReact />
+        <SiRedux />
+        <SiBootstrap />
+        <SiMaterialUi />
+        <FaAws />
+        <SiFirebase />
+        <SiNetlify />
+        <SiGit />
+        <AiFillGithub />
+      </div>
+      <p>I am Currently learning the following:</p>
+      <div className="tech-icons">
         <SiGatsby />
         <SiCsharp />
         <SiDotNet />
         <SiPython />
         <SiUnity />
-      </p>
+        <SiPostgresql />
+      </div>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolores,
         quasi nostrum delectus et voluptates laboriosam impedit libero quidem
