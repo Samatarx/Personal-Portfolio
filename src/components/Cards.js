03687af7e31525c909eeda2f87a9f44ff66f1bb4
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-04-30 17:00:35 +0100

    Created Card component and map()

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,36 @@
+import cardData from "./cardData";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
+import images from '../images'
 
-const Cards = () => {
-    return (
-        <summary className='cards-summary' >
-            <h2 className='sub-heading-cards' >Projects</h2>
-        </summary>
-    )
-}
+const Cards = ({ colour }) => {
+  return (
+    <summary className="cards cards-summary">
+      <h2 className="sub-heading-cards">Projects</h2>
+      {cardData.map((card, idx) => {
+        const { id, name, image, github, url, tech, desc } = card;
+        return (
+          <div className="project-card" key={idx}>
+            <p className="project-title">{name}</p>
+            {/* <p>{desc}</p> */}
+            <img src={images[0].img} alt={name}/>
+            <ul className="project-tech">
+              {tech.map((tec) => {
+                return <li className="project-tech-list">{tec}</li>;
+              })}
+            </ul>
+            <div className={`card-links ${colour}`}>
+              <a href={url} target="_blank" rel="noreferrer">
+                <AiOutlineLink />
+              </a>
+              <a href={github} target="_blank" rel="noreferrer">
+                <AiFillGithub />
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </summary>
+  );
+};
 
-export default Cards
+export default Cards;
