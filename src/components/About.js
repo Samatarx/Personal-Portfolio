474b714042a47d6caa5faca4747ac50b2def77f3
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-04-30 13:00:08 +0100

    Content and Styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,49 @@
 function About({ colour }) {
   return (
-    <summary>
+    <summary className="about-summary">
       <h2 className="sub-heading">About Me</h2>
-      <p className={`${colour}`}>
-        I am London based self-taught software developer looking for my first
-        graduate/junior role. I have specialised thus far in front end
-        technologies and I am currently learning backend engineering.
+      <p>
+        <span className={`${colour}`}>
+          I am London based self-taught software developer
+        </span>
+        looking for my first graduate/junior role. I have specialised thus far
+        in <span className={`${colour}`}> front end technologies</span> and I am
+        currently learning backend engineering.
       </p>
-      <p className={`${colour}`}>
+      <p>
         I started learning how to code on the 10<sup>th</sup> of July 2020 at
         the start of the pandemic, initially as a hobby. I've enjoyed it so much
-        that I have decided to pursue a career in web development.
+        that I have decided to pursue a
+        <span className={`${colour}`}> career in web development</span>.
       </p>
-      <p className={`${colour}`}>
+      <p>
         I am confident in my ability to build appealing and user friendly web
-        components with data fetched from APIs using React JS
+        components with data fetched from APIs using{" "}
+        <span className={`${colour}`}>React JS</span>
       </p>
       <h3 className="sub-heading">Education</h3>
       <h4>Certificates</h4>
-      <p className={`${colour}`}>
-        Responsive Web Design - FreeCodeCamp - Approxiametly 300 hours of
-        coursework
+      <p>
+        Responsive Web Design - FreeCodeCamp -{" "}
+        <span className={`${colour}`}>
+          Approxiametly 300 hours of coursework
+        </span>
       </p>
-      <p className={`${colour}`}>
-        JavaScript Algorithms and Data Structures - FreeCodeCamp - Approxiametly
-        300 hours of coursework
+      <p>
+        JavaScript Algorithms and Data Structures - FreeCodeCamp -{" "}
+        <span className={`${colour}`}>
+          Approxiametly 300 hours of coursework
+        </span>
       </p>
       <h4>Bachelors</h4>
-      <p className={`${colour}`}>
-        City University London - BEng Civil Engineering - 1st Class Honours
-        Degree
+      <p>
+        City University London - BEng Civil Engineering -{" "}
+        <span className={`${colour}`}> 1st Class Honours Degree</span>
       </p>
       <h4>Masters</h4>
-      <p className={`${colour}`}>
-        City University London - MSc Civil Engineering Structures - Distinction
+      <p>
+        City University London - MSc Civil Engineering Structures -{" "}
+        <span className={`${colour}`}> Distinction</span>
       </p>
     </summary>
   );
