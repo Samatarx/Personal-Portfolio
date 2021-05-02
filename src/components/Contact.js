7fbb316abac3035d364876f965bbfed625103268
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-05-02 16:42:48 +0100

    Contact page start

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,18 @@
-const Contact = () => {
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+
+const Contact = ({colour}) => {
     return (
-        <div>
-            <h2>Contact Me</h2>
+        <summary>
+            <h2 className='sub-heading' >Contact Me</h2>
+            <div className="quick-social">
+          <a href="https://www.linkedin.com/in/samatarxasan/" target="blank">
+            <AiFillLinkedin className={`${colour}`} />
+          </a>
+          <a href="https://github.com/samatarx" target="blank">
+            <AiFillGithub className={`${colour}`} />
+          </a>
         </div>
+        </summary>
     )
 }
 
