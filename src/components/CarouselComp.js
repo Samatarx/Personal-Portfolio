e1d17252d1526a7f78d90f0aeb3455264602ec64
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-05-04 15:17:00 +0100

    Carousel adjustment for responsiveness

--- src/components/CarouselComp.js
+++ src/components/CarouselComp.js
@@ -24,15 +24,15 @@ const CarouselComp = ({ colour }) => {
         spaceBetween={30}
         breakpoints={{
           1: {
-            slidesPerView: 1,
+            slidesPerView: 3,
             spaceBetween: 20,
           },
           640: {
-            slidesPerView: 1,
+            slidesPerView: 4,
             spaceBetween: 20,
           },
           768: {
-            slidesPerView: 5,
+            slidesPerView:4 ,
             spaceBetween: 50,
           },
           1024: {
@@ -60,12 +60,12 @@ const CarouselComp = ({ colour }) => {
                       </a>
                     )}
                     {github && (
-                      <a href={figma} target="_blank" rel="noreferrer">
+                      <a href={github} target="_blank" rel="noreferrer">
                         <AiFillGithub className={`${colour}`} />
                       </a>
                     )}
                     {site && (
-                      <a href={figma} target="_blank" rel="noreferrer">
+                      <a href={site} target="_blank" rel="noreferrer">
                         <AiOutlineLink className={`${colour}`} />
                       </a>
                     )}
