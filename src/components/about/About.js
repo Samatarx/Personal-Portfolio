b82919014d7cfebb22009f81e35dd83598abab0c
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-05-06 12:09:35 +0100

    htmlFor replacement

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -20,7 +20,7 @@ function About({ colour }) {
                 setSize(0);
               }}
             />
-            <label for="shortest">.</label>
+            <label htmlFor="shortest">.</label>
           </div>
           <div className="radio-div">
             <input
@@ -33,7 +33,7 @@ function About({ colour }) {
                 setSize(1);
               }}
             />
-            <label for="short">.</label>
+            <label htmlFor="short">.</label>
           </div>
           <div className="radio-div">
             <input
@@ -46,7 +46,7 @@ function About({ colour }) {
                 setSize(2);
               }}
             />
-            <label for="medium">.</label>
+            <label htmlFor="medium">.</label>
           </div>
           <div className="radio-div">
             <input
@@ -59,7 +59,7 @@ function About({ colour }) {
                 setSize(3);
               }}
             />
-            <label for="long">.</label>
+            <label htmlFor="long">.</label>
           </div>
         </div>
         <div className="labels">
